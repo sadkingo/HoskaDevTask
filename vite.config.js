6c8929ba5acaf7b8c6fe69b56c0353e213c1f175
sadkingo <hker0004@gmail.com>
2025-01-06 02:38:26 +0100

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                course: 'course.html',
+                header: 'src/components/header/header.html',
+                footer: 'src/components/footer/footer.html',
+            },
+        },
+    },
     resolve: {
         alias: {
             '@assets': '/src/assets',
